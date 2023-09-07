970db957f3a2c3a8fa84ff566156eda4c7d69db0
Serban Gherescu <sgherescu@yahoo.com>
2023-09-07 20:06:07 +0300

    packages created and setted application.properties

---
+++ src/test/java/com/example/ocp_be/OcpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
